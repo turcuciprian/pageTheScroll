8c0f36f5e681851272fa62603491cb64604d2729
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-06 10:07:04 +0300

    added and linked scrollview

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var images = [UIImageView]()
+    
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for x in 0...2{
+            let image = UIImage(named: "\(x).png")
+            images.append(UIImageView(image:image))
+        }
+        print("count: \(images.count)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
