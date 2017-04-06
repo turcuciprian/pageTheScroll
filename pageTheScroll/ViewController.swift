a2b0974ab8733b850c109b62fe5c1e73517a0920
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-06 11:13:33 +0300

    done - the task

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -16,6 +16,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        
         // Do any additional setup after loading the view, typically from a nib.
         for x in 0...2{
             let image = UIImage(named: "icon\(x).png")
@@ -24,13 +30,18 @@ class ViewController: UIViewController {
             
             var newX: CGFloat = 0.0
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX-75, y: (view.frame.size.height / 2)-75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX-75, y: (scrollView.frame.size.height / 2)-75, width: 150, height: 150)
         }
-        print("count: \(images.count)")
+        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
+    
 }
 
